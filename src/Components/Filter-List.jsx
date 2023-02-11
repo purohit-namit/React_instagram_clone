44ea5f20b1bf7951cf3deee632a467ec32db6b2c
Namit Purohit <purohit.namit@gmail.com>
2023-02-11 21:52:03 +0530

    Insta_commited

---
+++ src/Components/Filter-List.jsx
@@ -0,0 +1,39 @@
+export const FilterList = [
+  {
+    name: "None",
+    class: "",
+  },
+
+  {
+    name: "Aden",
+    class: "filter-aden",
+  },
+  {
+    name: "Amaro",
+    class: "filter-amaro",
+  },
+  {
+    name: "Ashby",
+    class: "filter-ashby",
+  },
+  {
+    name: "Brannan",
+    class: "filter-brannan",
+  },
+  {
+    name: "Brooklyn",
+    class: "filter-brooklyn",
+  },
+  {
+    name: "Charmes",
+    class: "filter-charmes",
+  },
+  {
+    name: "Clarendon",
+    class: "filter-clarendon",
+  },
+  {
+    name: "Crema",
+    class: "filter-crema",
+  },
+];
